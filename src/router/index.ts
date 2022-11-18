a407fdcc8b29625f3fe96ca398ba521d8a611fed
谢志非 <xiezf@91160.com>
2022-11-18 20:38:19 +0800

    fix: 优化路由逻辑

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import NProgress from 'nprogress'; // progress bar
-import 'nprogress/css/nprogress.css'; // 进度条样式
+import { basicRoutes } from './routes';
+import type { RouteRecordRaw } from 'vue-router';
+import type { App } from 'vue';
 
+import 'nprogress/css/nprogress.css'; // 进度条样式
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const routes: Array<RouteRecordRaw> = [
@@ -11,10 +14,34 @@ const routes: Array<RouteRecordRaw> = [
   },
 ];
 
+// 白名单应该包含基本静态路由
+const WHITE_NAME_LIST: string[] = [];
+const getRouteNames = (array: any[]) =>
+  array.forEach((item) => {
+    WHITE_NAME_LIST.push(item.name);
+    getRouteNames(item.children || []);
+  });
+getRouteNames(basicRoutes);
+
+// 创建一个可以被 Vue 应用程序使用的路由实例
 const router = createRouter({
-  history: createWebHashHistory(),
+  // 创建一个 hash 历史记录。
+  history: createWebHashHistory(import.meta.env.VITE_BASE_URL),
   routes,
+  // 是否应该禁止尾部斜杠。默认为假
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
+
+// reset router
+export function resetRouter() {
+  router.getRoutes().forEach((route) => {
+    const { name } = route;
+    if (name && !WHITE_NAME_LIST.includes(name as string)) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
+}
 
 router.beforeEach(() => {
   NProgress.start(); // start progress bar
@@ -24,5 +51,10 @@ router.beforeEach(() => {
 router.afterEach(() => {
   NProgress.done(); // finish progress bar
 });
+
+// 配置路由器
+export function setupRouter(app: App<Element>) {
+  app.use(router);
+}
 
 export default router;
